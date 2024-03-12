021b90af3485d42f239ae22310e84b8043e05e8b
tobilg <tobilg@gmail.com>
2024-03-12 10:31:46 +0100

    Fix bug

--- src/gatherFromHTML.ts
+++ src/gatherFromHTML.ts
@@ -12,6 +12,11 @@ export interface RuntimeInfo {
   blockFunctionUpdateDate: string | null;
 }
 
+const formatDate = (dateString: string): string => {
+  const d = new Date(dateString);
+  return new Date(d.getTime() - (d.getTimezoneOffset()*60*1000)).toISOString().split('T')[0]
+}
+
 const run = async () => {
   const html = await downloadAsHTML("https://docs.aws.amazon.com/lambda/latest/dg/lambda-runtimes.html");
 
@@ -26,8 +31,8 @@ const run = async () => {
         sdkVersion: tds[2].textContent?.trim() || "",
         operatingSystem: tds[3].textContent?.trim() || "",
         deprecationDate: tds[4].textContent?.trim() ? new Date(tds[4].textContent!.trim()).toISOString().substring(0, 10) : null,
-        blockFunctionCreateDate: tds[5].textContent?.trim() ? new Date(tds[5].textContent!.trim()).toISOString().substring(0, 10) : null,
-        blockFunctionUpdateDate: tds[6].textContent?.trim() ? new Date(tds[6].textContent!.trim()).toISOString().substring(0, 10) : null,
+        blockFunctionCreateDate: tds[5].textContent?.trim() ? formatDate(tds[5].textContent!.trim()) : null,
+        blockFunctionUpdateDate: tds[6].textContent?.trim() ? formatDate(tds[6].textContent!.trim()) : null,
       } as RuntimeInfo
     } else if (tds.length === 6) {
       return {
@@ -35,22 +40,14 @@ const run = async () => {
         identifier: tds[1].textContent?.trim() || "",
         sdkVersion: null,
         operatingSystem: tds[2].textContent?.trim() || "",
-        deprecationDate: tds[3].textContent?.trim() ? new Date(tds[3].textContent!.trim()).toISOString().substring(0, 10) : null,
-        blockFunctionCreateDate: tds[4].textContent?.trim() ? new Date(tds[4].textContent!.trim()).toISOString().substring(0, 10) : null,
-        blockFunctionUpdateDate: tds[5].textContent?.trim() ? new Date(tds[5].textContent!.trim()).toISOString().substring(0, 10) : null,
+        deprecationDate: tds[3].textContent?.trim() ? formatDate(tds[3].textContent!.trim()) : null,
+        blockFunctionCreateDate: tds[4].textContent?.trim() ? formatDate(tds[4].textContent!.trim()) : null,
+        blockFunctionUpdateDate: tds[5].textContent?.trim() ? formatDate(tds[5].textContent!.trim()) : null,
       } as RuntimeInfo
     } else {
-      return {
-        name: tds[0].textContent?.trim() || "",
-        identifier: tds[1].textContent?.trim() || "",
-        sdkVersion: null,
-        operatingSystem: tds[2].textContent?.trim() || "",
-        deprecationDate: tds[3].textContent?.trim() ? new Date(tds[3].textContent!.trim()).toISOString().substring(0, 10) : null,
-        blockFunctionCreateDate: tds[4].textContent?.trim() ? new Date(tds[4].textContent!.trim()).toISOString().substring(0, 10) : null,
-        blockFunctionUpdateDate: null,
-      } as RuntimeInfo
+      return null;
     }
-  })
+  }).filter((runtimeInfo) => runtimeInfo);
 
   writeFileSync(join(__dirname, "../", "data", "lambdaRuntimes.json"), JSON.stringify(data.sort((a, b) => a.identifier > b.identifier ? 0 : -1), null, 2));
 }
